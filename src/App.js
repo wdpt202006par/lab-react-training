dc870e0366cfea69d081e3139afe5ba6da626294
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2020-10-22 21:33:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import IdCard from './idcard/IdCard'
 
 function App() {
   return (
@@ -19,6 +20,24 @@ function App() {
           Learn React
         </a>
       </header>
+      <IdCard
+        lastName='Doe'
+        firstName='John'
+        gender='male'
+        height={178}
+        birth={new Date("1992-07-14")}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+
+      <IdCard
+        lastName='Delores '
+        firstName='Obrien'
+        gender='female'
+        height={172}
+        birth={new Date("1988-05-11")}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
+
     </div>
   );
 }
