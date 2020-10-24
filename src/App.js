a9412aa64e8abc93b26042ee31b38eb6911e1c79
Sarah Damag <sarahdamag@gmail.com>
2020-10-24 10:35:09 +0200

    with iteration 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function IdCard(props){
+  return (
+    <div class="idCard">
+      <div>
+        <img src={props.picture} alt="p"></img>
+      </div>
+      <div>
+        <p>First name:{props.firstName}</p>
+        <p>Last name:{props.lastName}</p>
+        <p>Gender:{props.gender}</p>
+        <p>Height:{props.height}</p>
+        <p>Birth:{props.birth}</p>
+      </div>
+    </div>)
+}
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +34,8 @@ function App() {
         >
           Learn React
         </a>
+        <IdCard picture="https://randomuser.me/api/portraits/men/44.jpg" firstName ="John" lastName="Doe" gender="male" height="1.78m" birth="Tue Jul 15 1992"/>
+        <IdCard picture="https://randomuser.me/api/portraits/women/44.jpg" firstName ="Obrien" lastName="Dolored" gender="female" height="1.72m" birth="Tue May 12 1993"/>
       </header>
     </div>
   );
