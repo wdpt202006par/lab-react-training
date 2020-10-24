26d3b602dc2d5e50a22665bff03e60feac65469c
Marine Tomazi <m.tomazi@hotmail.fr>
2020-10-24 10:20:32 +0200

    iteration 1 on going

--- src/App.js
+++ src/App.js
@@ -24,18 +24,18 @@ function App() {
            firstName='John'
            gender='male'
            height={178}
-           birth={new Date("1992-07-14")}
+           birth="1992-07-14"
            picture="https://randomuser.me/api/portraits/men/44.jpg"
         />
 
-        <IdCard
+        {/* <IdCard
            lastName='Delores '
            firstName='Obrien'
            gender='female'
            height={172}
            birth={new Date("1988-05-11")}
            picture="https://randomuser.me/api/portraits/women/44.jpg"
-           />
+           /> */}
       </header>
     </div>
   );
--- src/IdCard/IdCard.js
+++ src/IdCard/IdCard.js
@@ -3,17 +3,14 @@ import React from 'react';
 
 const Idcard = props => {
     return ( 
-        <div>
-<IdCard
+        <div className ="id">
   lastName={props.lastName}
   firstName={props.firstName}
   gender={props.gender}
   height={props.height}
   birth={props.birth}
   picture={props.picture}
-/>
-        </div>
-        
+        </div>  
     )
 }
 
