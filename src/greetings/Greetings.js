ac1a9f694850b63cd4ca9dcbd610ced09901285e
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2020-10-24 11:02:02 +0200

    iteration 3 done

--- src/greetings/Greetings.js
+++ src/greetings/Greetings.js
@@ -1,27 +1,29 @@
 import React from 'react'
 
 const langueGreetings = {
-fr: 'Bonjour',
-de: 'Hallo',
-es: 'Hola',
-en:'Hello'
+    fr: 'Bonjour',
+    de: 'Hallo',
+    es: 'Hola',
+    en: 'Hello'
 }
 
-let result = "";
+// let result = "";
 
-function Greetings (props) {
-    var arrayGreetings = Object.keys(langueGreetings);
-    console.log(arrayGreetings);
-    console.log(props.lang);
-    arrayGreetings.forEach(el => {
-        if (props.lang === el){
-            result = langueGreetings[el] + " " + props.children + "";
-            console.log(result)
-        }
-    })
+function Greetings(props) {
+    //     var arrayGreetings = Object.keys(langueGreetings);
+    //     console.log(arrayGreetings);
+    //     console.log(props.lang);
+    //     arrayGreetings.forEach(el => {
+    //         if (props.lang === el){
+    //             result = langueGreetings[el] + " " + props.children + "";
+    //             console.log(result)
+    //         }
+    //     })
 
-    return(<div>
-       <p>{result}</p>
+
+
+    return (<div>
+        <p>{langueGreetings[props.lang]} {props.children}</p>
     </div>)
 }
 
