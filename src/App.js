7474a2f9bee87d69f1dd75f3a9b7c44428a1bad3
Claire Lacanal <lacanal.claire@gmail.com>
2020-10-24 10:37:34 +0200

    ite 1 done

--- src/App.js
+++ src/App.js
@@ -6,19 +6,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <IdCard
            lastName='Doe'
            firstName='John'
@@ -27,16 +14,14 @@ function App() {
            birth="1992-07-14"
            picture="https://randomuser.me/api/portraits/men/44.jpg"
         />
-
-        {/* <IdCard
+         <IdCard
            lastName='Delores '
            firstName='Obrien'
            gender='female'
            height={172}
-           birth={new Date("1988-05-11")}
+           birth="1988-05-11"
            picture="https://randomuser.me/api/portraits/women/44.jpg"
-           /> */}
-      </header>
+           /> 
     </div>
   );
 }
--- src/IdCard/IdCard.js
+++ src/IdCard/IdCard.js
@@ -4,12 +4,12 @@ import React from 'react';
 const Idcard = props => {
     return ( 
         <div className ="id">
-  lastName={props.lastName}
-  firstName={props.firstName}
-  gender={props.gender}
-  height={props.height}
-  birth={props.birth}
-  picture={props.picture}
+  lastName={props.lastName}<br/>
+  firstName={props.firstName}<br/>
+  gender={props.gender}<br/>
+  height={props.height}<br/>
+  birth={props.birth}<br/>
+  picture <img src={props.picture}></img><br/>
         </div>  
     )
 }
