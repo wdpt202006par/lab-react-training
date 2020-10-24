499d69ad967b4bbf668c22bfa82b9e1b12f51ec0
Claire Lacanal <lacanal.claire@gmail.com>
2020-10-24 12:06:33 +0200

    ite 5 on going

---
+++ src/CreditCard/CreditCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CreditCard = props => {
+    let divStyle = {
+        color: {props.color},
+        backgroundColor: {props.bgColor};
+
+    }
+    return(
+
+    )
+}
+
+export default CreditCard;
