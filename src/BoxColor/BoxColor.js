fbe16991489fed484a8973807b85354dd0ac66a9
Marine Tomazi <m.tomazi@hotmail.fr>
2020-10-25 23:25:03 +0100

    iteration 5 done sans css

--- src/BoxColor/BoxColor.js
+++ src/BoxColor/BoxColor.js
@@ -3,6 +3,8 @@ import React from 'react';
 const BoxColor = props => {
  let divStyle ={
      backgroundColor:`rgb(${props.r},${props.g},${props.b})`,
+     height:150,
+     width: 500,
  };
     return (
         <div style={divStyle}>
--- src/CreditCard/CreditCard.js
+++ src/CreditCard/CreditCard.js
@@ -1,13 +1,22 @@
+import { findByLabelText } from '@testing-library/react';
 import React from 'react';
 
 const CreditCard = props => {
     let divStyle = {
-        color: {props.color},
-        backgroundColor: {props.bgColor};
+        color:props.color,
+        backgroundColor: props.bgColor,
+        height: 200,
+        width: 400,
+        borderRadius:12,
+    };
 
-    }
     return(
-
+<div style={divStyle}>
+            <p>{props.type}</p>
+            <p>{props.number}</p>
+            <p>Expire {props.expirationMonth}/{props.expirationYear} {props.bank} </p>
+            <p>{props.owner}</p>
+        </div>
     )
 }
 
