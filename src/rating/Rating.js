768e87b66c1f1f9ab735d8e9beb8de7acdb84eb7
Claire Lacanal <lacanal.claire@gmail.com>
2020-10-26 21:33:34 +0100

    ite 6 done

--- src/rating/Rating.js
+++ src/rating/Rating.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
-const rating = props => {
+const Rating = props => {
+  let stars;
+  if(props.children <= 0.5 ) {
+    stars = '☆☆☆☆☆'
+  }else if(props.children >= 0.5 && props.children < 1.5 ) {
+    stars = '★☆☆☆☆'
+  }else if(props.children >= 1.5 && props.children < 2 ) {
+    stars = '★★☆☆☆'
+  }else if(props.children >= 1 && props.children <= 3.5) {
+    stars = '★★★☆☆'
+  }else if(props.children >= 3.5 && props.children <= 4.5) {
+    stars = '★★★★☆'
+  }else if(props.children > 4.5) {
+    stars = '★★★★★'
+  }
   
     return(
         <div>
-          {props.children.repeat(Math.ceil(props.children))}
+           {(Math.round(props.children))} : {stars}
         </div>
     )
 }
 
-export default rating;
+export default Rating;
